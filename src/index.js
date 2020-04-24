733dfa81bec787f12174c66bc95292a03a46c095
devjeanurzedo <jean.urzedo@gmail.com>
2020-04-24 08:46:48 -0400

    Refatoração index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 'use strict'
 
 const fibonacci = () => {
-    let fibarray = [];
+    let fibarray = [0,1];
     let next = 0;
 
     for (let index = 0; next < 350; index ++){
-
-        if (index < 2){
-            fibarray.push(index)
-            continue
-        }
-
         next = fibarray[fibarray.length - 1] + fibarray[fibarray.length -2];
         fibarray.push(next);
     }
+    
     return fibarray;
 }
 
